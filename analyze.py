bbf4ccb3f66fa4d3037bd42d2eccabf80ea36ad5
OrenGitHub <tuna.is.good.for.you@gmail.com>
2024-03-31 14:17:54 +0300

    extracting tar-ed docker image

--- analyze.py
+++ analyze.py
@@ -1,56 +1,52 @@
+import os
 import json
 import glob
+import tarfile
 import requests
 
-# this is my lovely new comment
+# dev imports
+from typing import Final
 
-def get_python_src_filenames() -> list[str]:
-    return glob.glob('code_tag/*.py', recursive=True)
+# routes
+TO_NATIVE_JS_AST_ROUTE: Final[str] = 'http://localhost:8000/to/native/ast'
+TO_DHSCANNER_AST_ROUTE: Final[str] = 'http://localhost:8000/to/dhscanner/ast'
 
-def get_python_src_files() -> dict[str, str]:
-	python_src_files = {}
-	filenames = get_python_src_filenames()
-	for filename in filenames:
-		with open(filename) as fl:
-			python_code = fl.read()
-			python_src_files[filename] = python_code
+def get_src_filenames(dirname: str, suffix: str) -> list[str]:
+    return glob.glob(f"{dirname}/*.{suffix}", recursive=True)
 
-	return python_src_files
+def get_src_files(dirname: str, suffix: str) -> dict[str, str]:
+    src_files = {}
+    filenames = get_src_filenames(suffix)
+    for filename in filenames:
+        with open(filename) as fl:
+            code = fl.read()
+            src_files[filename] = code
 
-def translate(filename: str, python_code: str) -> str:
-    files = {'source': ('source', python_code)}
-    response = requests.post('http://localhost:8000/translator/python', files=files)
-    # print(f'translation: {filename} -> {response.status_code}')
-    #if 'oren' in filename:
-    #    print(response.text)
+    return python_src_files
+
+def to_native_js_ast(js_code: str) -> str:
+    files = {'source': ('source', js_code)}
+    response = requests.post(TO_NATIVE_JS_AST_ROUTE, files=files)
     return response.text
 
-def parse(filename:str, python_code: str):
-    code = {'filename': filename, 'content': python_code}
-    response = requests.post('http://localhost:8001', json=code)
-    # print(f'parsing:     {filename} -> {response.status_code}')
+def from_js_ast_to_dhscanner_ast(js_filename:str, js_code: str):
+    code = {'filename': js_filename, 'content': js_code}
+    response = requests.post(TO_DHSCANNER_AST_ROUTE, json=code)
     return response.json()
 
-def get_asts():
+def get_asts(dirname: str, suffix: str, to_native_ast, to_dhscanner_ast):
 
     asts = []
-    python_src_files = get_python_src_files()
-    for filename, python_code in python_src_files.items():
-        translated = translate(filename, python_code)
-        asts.append(parse(filename, translated))
+    src_files = get_src_files(dirname, suffix)
+    for filename, code in src_files.items():
+        native_ast = to_native_ast(filename, code)
+        asts.append(to_dhscanner_ast(filename, native_ast))
 
     return asts
 
-def get_callables():
+def analyze():
 
-    asts = {'dirname': 'someDir', 'astsContent': get_asts() }
-    response = requests.post('http://localhost:8002', json=asts)
-    return response.json()
-
-def main() -> None:
-
-    #callables = get_callables()
-    asts = get_asts()
+    asts = get_asts('dockers/example_00', 'js', to_nativ_js_ast, from_native_js_ast_to_dhscanner_ast)
     for ast in asts:
         content = json.loads(json.dumps(ast))
         if isinstance(content, dict) and 'filename' in content:
@@ -59,10 +55,30 @@ def main() -> None:
         else:
             print(json.dumps(ast))
 
-    #callables = get_callables()
-    #response = requests.post('http://localhost:8003', json=callables)
-    #print(response.json())
+def main():
+
+    workdir = 'dockers/example_00'
+    tar_ed_docker = tarfile.open(name=f'{workdir}/example.tar')
+    tar_ed_docker.extractall(path=workdir)
+    tar_ed_docker.close()
+
+    dirnames = []    
+    layers = glob.glob(f'{workdir}/**/layer.tar', recursive=True)
+    for layer in layers:
+        tar_ed_layer = tarfile.open(name=layer)
+        dirname = os.path.dirname(layer)
+        tar_ed_layer.extractall(path=dirname)
+        tar_ed_layer.close()
+        dirnames.append(dirname)
+
+    print(dirnames)
+
+    for dirname in dirnames:
+        js_files = glob.glob(f'{dirname}/**/*.js', recursive=True)
+        for js_file in js_files:
+            if 'node_modules' not in js_file:
+                print(js_file)
 
 if __name__ == "__main__":
-	main()
+    main()
 
